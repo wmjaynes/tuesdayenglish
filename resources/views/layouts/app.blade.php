c582337cf06356c2e7f9a21e22eeeee913642229
wmjaynes <will@jaynes.org>
2019-08-08 12:07:07 -0400

    Use versioning in Laravel mix file.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,13 +10,13 @@
 {{--    <title>{{ config('app.name', 'AACTMAD Tuesday English') }}</title>--}}
     <title>AACTMAD Tuesday English</title>
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
     <div id="app">
